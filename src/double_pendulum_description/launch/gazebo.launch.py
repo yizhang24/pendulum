c3cf63b8ccf8fb15e7a6511d5f4c31740385e57f
Xuanyi Zhang <58002595+yizhang24@users.noreply.github.com>
2025-11-08 23:20:50 -0500

    Bridge pendulum states to ros

--- src/double_pendulum_description/launch/gazebo.launch.py
+++ src/double_pendulum_description/launch/gazebo.launch.py
@@ -1,26 +1,44 @@
 from launch import LaunchDescription
 from launch.actions import ExecuteProcess
+from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 import os
 
 from launch.actions.set_environment_variable import SetEnvironmentVariable
 
+
 def generate_launch_description():
     world_path = os.path.join(
-        get_package_share_directory('double_pendulum_description'),
-        'worlds', 'double_pendulum.world'
+        get_package_share_directory("double_pendulum_description"),
+        "worlds",
+        "double_pendulum.world",
     )
     model_dir = os.path.join(
-        get_package_share_directory('double_pendulum_description'),
-        'models'
+        get_package_share_directory("double_pendulum_description"), "models"
     )
-    return LaunchDescription([
-      SetEnvironmentVariable(
-          name='GZ_SIM_RESOURCE_PATH',
-          value=f'/opt/ros/kilted/share:{model_dir}'
-      ),
-      ExecuteProcess(
-        cmd=['gz', 'sim', '-v', '4', f'{world_path}', '--render-engine', 'ogre'],
-        output='screen'
-      )
-    ])
+    return LaunchDescription(
+        [
+            SetEnvironmentVariable(
+                name="GZ_SIM_RESOURCE_PATH", value=f"/opt/ros/kilted/share:{model_dir}"
+            ),
+            ExecuteProcess(
+                cmd=[
+                    "gz",
+                    "sim",
+                    "-v",
+                    "4",
+                    f"{world_path}",
+                    "--render-engine",
+                    "ogre",
+                ],
+                output="screen",
+            ),
+            Node(
+                package="ros_gz_bridge",
+                executable="parameter_bridge",
+                arguments=[
+                    "/world/double_pendulum_world/model/double_pendulum/joint_state@sensor_msgs/msg/JointState@gz.msgs.Model",
+                ],
+            ),
+        ]
+    )
